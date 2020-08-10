d757ee57b1e08a93a3985c92233eb1f4363e2fc6
Aswini Kumar <aswinikumar448@gmail.com>
2020-08-10 16:44:31 +0530

    change

--- src/components/User.js
+++ src/components/User.js
@@ -6,7 +6,7 @@ export default function User(props) {
        <span>Name: {props.name} , age: {props.age} </span>
        <input onChange={props.changeEvent} value={props.name}/>
         <button onClick={props.deleteEvent}>delete</button>
-        
+        <div>aswin</div>
    </li>
   )
 }
